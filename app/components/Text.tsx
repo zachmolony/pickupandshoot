ea015cde35f91baefacb8cdc0708d68d512c8bd0
Zach Molony <31472522+zachmolony@users.noreply.github.com>
2023-11-27 13:45:07 +0000

    make text component

---
+++ app/components/Text.tsx
@@ -0,0 +1,30 @@
+import {Link} from '@remix-run/react';
+
+const Text = ({colour = 'default', children}) => {
+  const col = {
+    default: 'text-white',
+    white: 'bg-white text-slate-700 text-center',
+    green: 'bg-lime-400 text-slate-700 w-9/12',
+  };
+  return (
+    <div style={{height: '1.3rem'}} className={`px-2 w-full ` + col[colour]}>
+      <span>{children}</span>
+    </div>
+  );
+};
+
+// const TextLink = ({link}) => {
+//   return (
+
+//     <Link
+//       className="collection-item"
+//       key={collection.id}
+//       to={link}
+//       prefetch="intent"
+//     >
+//       <h5>{childer}</h5>
+//     </Link>
+//   )
+// }
+
+export default Text;
